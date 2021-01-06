b0512d82df92d4baa8cad596929808d74dbc6d9f
Sérgio Piacenti <sergio.piacenti@itixti.com.br>
2021-01-06 09:08:49 -0300

    Add project files.

--- DesafioItix/Startup.cs
+++ DesafioItix/Startup.cs
@@ -1,15 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using DesafioItix.Data;
 
@@ -24,7 +17,6 @@ namespace DesafioItix
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -43,7 +35,6 @@ namespace DesafioItix
             services.AddScoped(typeof(IDataRepository<>), typeof(DataRepository<>));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -52,7 +43,6 @@ namespace DesafioItix
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
